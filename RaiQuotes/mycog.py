4c65c46dc1c7586c9202e44db626ea459bc0c4d7
glugg12 <olijordan55@googlemail.com>
2020-09-16 06:25:08 +0100

    Update mycog.py

--- RaiQuotes/mycog.py
+++ RaiQuotes/mycog.py
@@ -55,54 +55,6 @@ class Mycog(commands.Cog):
         finally:
             if conn:
                 conn.close()
-    
-    @commands.command()
-    async def randomquote(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        random.seed(datetime.now())
-
-        conn = None
-        try:
-            conn = sqlite3.connect(r"quotes.sqlite")
-
-            cur = conn.cursor()
-            count = 0
-            cur.execute("SELECT * FROM quotes")
-            rows = cur.fetchall()
-            for row in rows:
-                if row[1] == ctx.message.guild.id:
-                    count = count + 1
-            randval = randint(0,count)
-            name = 'Error'
-            url = ''
-            addedby = '?'
-            check = 0
-            for row in rows:
-                if row[1] == ctx.message.guild.id:
-                    if '{}'.format(check) == '{}'.format(randval):
-                        name = '{}'.format(row[7])
-                        for member in ctx.message.guild.members:
-                            if row[6] == member.id:
-                                name = '{}'.format(member.display_name)
-                                url = member.avatar_url
-                            if row[5] == member.id:
-                                addedby = '{}'.format(member.display_name)
-                        emb = discord.Embed(title='{}'.format(name), description='{}'.format(row[8]), colour = 0x00ff00)
-                        emb.set_footer(text = 'Added by: {} | Quote ID: {}'.format(addedby, row[2]))
-                        if row[10] != None:
-                            emb.set_image(url='{}'.format(row[10]))
-                        emb.set_thumbnail(url='{}'.format(url))
-                        await ctx.channel.send(embed=emb)
-                    check = check + 1
-            
-            
-            
-        except Error as e:
-            print(e)
-        finally:
-            if conn:
-                conn.close()
 
     @commands.command()
     async def addquote(self, ctx, author):
