02a6d1df4029eaade79be4600e9c4bd5827617b8
glugg12 <olijordan55@googlemail.com>
2022-06-05 00:06:47 +0100

    Update mycog.py

--- RaiQuotes/mycog.py
+++ RaiQuotes/mycog.py
@@ -7,7 +7,7 @@ from random import seed
 from random import randint
 from datetime import datetime
 client = discord.Client()
-path = '%LOCALAPPDATA%\Red-DiscordBot\Red-DiscordBot\data\Springfield\cogs\CogManager\cogs\RaiQuotes\quotes.sqlite'
+path = "%LOCALAPPDATA%\Red-DiscordBot\Red-DiscordBot\data\Springfield\cogs\CogManager\cogs\RaiQuotes\quotes.sqlite"
 class Mycog(commands.Cog):
     """My custom cog"""
 
