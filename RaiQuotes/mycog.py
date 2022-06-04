44fdeccef534f18cdc168c1faeafb41124ac142f
glugg12 <olijordan55@googlemail.com>
2022-06-05 00:34:13 +0100

    Update mycog.py

--- RaiQuotes/mycog.py
+++ RaiQuotes/mycog.py
@@ -305,4 +305,4 @@ class Mycog(commands.Cog):
     @commands.command()
     async def raihepl(self, ctx):
         """Help command"""
-        await ctx.channel.send('```Here are the commands for RaiQuotes cog!\nquoteid[id] | Show the quote at [id]\naddqoute [author] [quote] | Add a new quote to the database/ Accepts discord @user for [author] too!\ndeleteid [id] | Deletes quote at [id]. It will be gone.... forever....\nrandom | Shows a random quote\ntotal [author] | Shows how many quotes [author] has in this server\ngrandtotal | Shows the total quotes in the server```'
+        await ctx.channel.send('```Here are the commands for RaiQuotes cog!\nquoteid[id] | Show the quote at [id]\naddqoute [author] [quote] | Add a new quote to the database/ Accepts discord @user for [author] too!\ndeleteid [id] | Deletes quote at [id]. It will be gone.... forever....\nrandom | Shows a random quote\ntotal [author] | Shows how many quotes [author] has in this server\ngrandtotal | Shows the total quotes in the server```')
