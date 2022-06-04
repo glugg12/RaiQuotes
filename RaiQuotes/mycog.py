97742084c6df6ed892914a917b2219b2cace0732
glugg12 <olijordan55@googlemail.com>
2022-06-04 23:45:40 +0100

    aaaa

--- RaiQuotes/mycog.py
+++ RaiQuotes/mycog.py
@@ -7,7 +7,7 @@ from random import seed
 from random import randint
 from datetime import datetime
 client = discord.Client()
-
+path = '%LOCALAPPDATA%\Red-DiscordBot\Red-DiscordBot\data\Springfield\cogs\CogManager\cogs\RaiQuotes\quotes.sqlite'
 class Mycog(commands.Cog):
     """My custom cog"""
 
@@ -18,7 +18,7 @@ class Mycog(commands.Cog):
         
         conn = None
         try:
-            conn = sqlite3.connect(r"%LOCALAPPDATA%\Red-DiscordBot\Red-DiscordBot\data\Springfield\cogs\CogManager\cogs\RaiQuotes\quotes.sqlite")
+            conn = sqlite3.connect(path)
 
             cur = conn.cursor()
             cur.execute("SELECT * FROM quotes")
