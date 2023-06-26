8397169482b61011746b922451e176dd0ad0faf6
Oli <olijordan55@googlemail.com>
2023-06-26 17:15:58 +0100

    bweeeh

--- RaiSearch/mycog.py
+++ RaiSearch/mycog.py
@@ -80,7 +80,7 @@ class Searchcog(commands.Cog):
     async def next(self, ctx):
 
         if last_query:
-            command = get_command('docs')
+            command = self.get_command('docs')
             await ctx.invoke(command, *last_query, index=last_index+1)
 
         else:
