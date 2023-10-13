992f055382f54666d570beecb7aed4d7cd3bbce8
glugg12 <olijordan55@googmail.com>
2023-10-13 18:54:35 +0100

    v2 start

--- RaiQuotesV2/mycog.py
+++ RaiQuotesV2/mycog.py
@@ -18,7 +18,7 @@ config = configparser.ConfigParser()
 
 config.read(configPath)
 # need to run api alongside this
-apiUrl = 'http://localhost:8080/'
+apiUrl = 'http://api:8080/'
 
 
 class Mycog(commands.Cog):
