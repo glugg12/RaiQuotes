d2a95ccdc0836f897d26921d4c552b36fa3a904f
glugg12 <olijordan55@googmail.com>
2023-10-13 17:51:30 +0100

    v2 start

---
+++ RaiQuotesV2/mycog.py
@@ -0,0 +1,95 @@
+import json
+
+from redbot.core import commands
+import discord
+import sqlite3
+from sqlite3 import Error
+import random
+from random import seed
+from random import randint
+from datetime import datetime
+import requests
+import configparser
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+path = r"D:\Springfield\cogs\RaiQuotes\quotes.sqlite"
+configPath = r"D:\Springfield\cogs\RaiQuotes\ApiConfig.ini"
+config = configparser.ConfigParser()
+
+
+
+# testing path
+# path = r"C:\Users\olijo\Documents\discordRedbot\quotes.sqlite"
+#configPath = r"C:\Users\olijo\Documents\discordRedbot\ApiConfig.ini"
+config.read(configPath)
+apiUrl = config['DEFAULT']['Api']
+
+class Mycog(commands.Cog):
+    """RaiQuotes Cog"""
+
+    @commands.command()
+    async def quoteid(self, ctx, quote_id):
+        """Finds a quote at the requested id"""
+        server_id = ctx.message.guild.id
+        if isinstance(quote_id, int):
+            await ctx.channel.send("That is not a valid integer")
+            return
+        else:
+            url = "localhost:8080/quotes/server/{}/{}".format(server_id, quote_id)
+            response = requests.post(url)
+            await ctx.channel.send(json.loads(response.content)["quote"])
+
+
+    @commands.command()
+    async def addquote(self, ctx, author):
+        """Adds a quote to the database"""
+
+    @commands.command()
+    async def random(self, ctx):
+        """Shows a random quote"""
+
+
+    @commands.command()
+    async def deleteid(self, ctx, word):
+        """Deletes a quote at the requested id"""
+
+    @commands.command()
+    async def total(self, ctx, author):
+        """Counts how many quotes the requested author has"""
+
+    @commands.command()
+    async def grandtotal(self, ctx):
+        """Counts how many quotes are in the server"""
+
+    @commands.command()
+    async def searchQuotes(self, ctx, word):
+        """Search for all quotes containing a word. Print's a table, may not show entirerty of longer quotes"""
+
+
+    @commands.command()
+    async def searchQuotesStrict(self, ctx, word):
+        """Search for all quotes containing a word. Print's a table, may not show entirerty of longer quotes"""
+
+
+    @commands.command()
+    async def remix(self, ctx):
+        """Remix baybeee"""
+
+
+    @commands.command()
+    async def remixid(self, ctx, id):
+        """Remix baybeee"""
+
+
+    @commands.command()
+    async def totalAdded(self, ctx, author):
+        """Counts how many quotes the requested author has added"""
+
+
+    @commands.command()
+    async def raihepl(self, ctx):
+        """More detailed help command"""
+        await ctx.channel.send(
+            '```Here are the commands for RaiQuotes cog!\nquoteid[id]               | Show the quote at [id]\naddquote [author] [quote] | Add a new quote to the database/ Accepts discord @user for [author] too!\ndeleteid [id]             | Deletes quote at [id]. It will be gone.... forever....\nrandom                    | Shows a random quote\ntotal [author]            | Shows how many quotes [author] has in this server\ngrandtotal                | Shows the total quotes in the server```')
+
