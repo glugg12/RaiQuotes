871459b8642f8571323108774039dac0df16d960
glugg12 <olijordan55@googmail.com>
2024-05-31 16:36:38 +0100

    test slash command

--- RaiQuotes/mycog.py
+++ RaiQuotes/mycog.py
@@ -687,9 +687,9 @@ class Mycog(commands.Cog):
                 conn.close()
 
     @app_commands.command()
-    async def raihepl(self, ctx):
+    async def raihepl(self, interaction: discord.Interaction):
         """More detailed help command"""
-        await ctx.channel.send(
+        await interaction.response.send_message(
             '```Here are the commands for RaiQuotes cog!\nquoteid[id]               | Show the quote at [id]\naddquote [author] [quote] | Add a new quote to the database/ Accepts discord @user for [author] too!\ndeleteid [id]             | Deletes quote at [id]. It will be gone.... forever....\nrandom                    | Shows a random quote\ntotal [author]            | Shows how many quotes [author] has in this server\ngrandtotal                | Shows the total quotes in the server```')
 
     # @commands.command()
