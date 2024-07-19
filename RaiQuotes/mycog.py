b95d0085f87cc469b4a1cb631a748142b0556c97
glugg12 <olijordan55@googmail.com>
2024-07-19 21:47:24 +0100

    bug huntin

--- RaiQuotes/mycog.py
+++ RaiQuotes/mycog.py
@@ -315,7 +315,7 @@ class Mycog(commands.Cog):
                         chop = int(len(q1) / 2) - 1
                 else:
                     chop = int(len(q1) / 2) - 1
-                while q1[chop] != ' ' and chop <= len(q1):
+                while q1[chop] != ' ' and chop <= len(q1) - 1:
                     chop = chop + 1
                     print(chop)
                 if chop < len(q1):
