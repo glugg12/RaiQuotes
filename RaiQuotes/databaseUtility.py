5216d30794f3772efd6f2584439cc289cf5ac3de
glugg12 <olijordan55@googmail.com>
2024-07-19 21:09:33 +0100

    delete splits

--- RaiQuotes/databaseUtility.py
+++ RaiQuotes/databaseUtility.py
@@ -154,16 +154,16 @@ def add_quote_splits(quote_id, server_id, left_split_end, right_split_start):
                     cur.execute(split_ex, (left_split_end, right_split_start, quote[0],))
                     updated_record = True
                 elif left_split_end is not None:
-                    if left_split_end > len(quote[8]):
-                        left_split_end = len(quote[8])
+                    if left_split_end > len(quote[8]) - 1:
+                        left_split_end = len(quote[8]) - 1
                     if left_split_end < 0:
                         left_split_end = 0
                     split_ex = '''UPDATE remix_split SET left_split_end = ? WHERE quote_id = ?'''
                     cur.execute(split_ex, (left_split_end, quote[0],))
                     updated_record = True
                 elif right_split_start is not None:
-                    if right_split_start > len(quote[8]):
-                        right_split_start = len(quote[8])
+                    if right_split_start > len(quote[8]) - 1:
+                        right_split_start = len(quote[8]) - 1
                     if right_split_start < 0:
                         right_split_start = 0
                     split_ex = '''UPDATE remix_split SET right_split_start = ? WHERE quote_id = ?'''
